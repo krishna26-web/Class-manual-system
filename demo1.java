8ba1cc02b66db8d9eb16300769dcaeb65c1e2891
krishna-26 <krishna-26>
2019-12-06 12:57:06 +0530

    Add wip files

---
+++ demo1.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class demo1
+{
+		
+		
+
+	public static void main(String[] arg)
+	{
+		String name,course;
+		int rollno;
+		
+		Scanner sc = new Scanner(System.in);
+		int choice =2;
+		
+		System.out.println("Student Record Menu");
+        System.out.println("1. Add Student");
+		System.out.println("2. Exit");
+		
+		  System.out.print("Enter Name: ");
+          name=sc.nextLine();
+          System.out.print("Enter Roll Number: ");
+          rollno=sc.nextInt();
+		  System.out.print("Enter Couser name: ");
+          name=sc.nextLine();
+		
+		
+		
+	}
+}
